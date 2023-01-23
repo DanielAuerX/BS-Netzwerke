38d361eeb78e54e9f1a790ff5336847e570db93a
REISST <T.Reiss@eos-ts.com>
2023-01-23 11:08:25 +0100

    CALCULATOR CHANGES MEOW :3
    updated calculator

--- networkPlan_Backend/src/main/java/com/itech/networkPlan/controller/CalculatorController.java
+++ networkPlan_Backend/src/main/java/com/itech/networkPlan/controller/CalculatorController.java
@@ -3,115 +3,106 @@ package com.itech.networkPlan.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/calculator")
 @CrossOrigin
 public class CalculatorController {
 
+    static HashMap<String, String> subnets = new HashMap<>();
+
     record NewCalculateRequest(String ip, String mask, int amount) {
     }
 
     @PostMapping()
-    public ResponseEntity<List<String>> doSomething(@RequestBody NewCalculateRequest request){
-        String ipAddress = request.ip();
-        String subnetMask = request.mask();
-        int numSubnets = request.amount();
-        List<String> testSubnets = calculateSubnets(ipAddress, Integer.parseInt(subnetMask), numSubnets);
-        /*
-        List<String> subnets = new ArrayList<>();
-        try {
-            InetAddress inetAddress = InetAddress.getByName(ipAddress);
-            InetAddress subnet = InetAddress.getByName(subnetMask);
-
-            byte[] ip = inetAddress.getAddress();
-            for(byte x: ip){
-                System.out.println(x);
-            }
-            byte[] mask = subnet.getAddress();
-
-            int maskBitLength = 0;
-            for (byte b : mask) {
-                int maskByte = b & 0xFF;
-                while (maskByte != 0) {
-                    maskBitLength += (maskByte & 1);
-                    maskByte >>>= 1;
-                }
-            }
-
-            byte[] subnetIP = new byte[ip.length];
-            for (int i = 0; i < ip.length; i++) {
-                subnetIP[i] = (byte) (ip[i] & mask[i]);
-            }
-
-            int startIndex = ip[ip.length-1] & 0xff;
-            for (int i = 0; i < numSubnets; i++) {
-                byte[] subnetAddr = new byte[ip.length];
-                for (int j = 0; j < ip.length-1; j++) {
-                    subnetAddr[j] = (byte) (subnetIP[j]);
-                }
-                subnetAddr[ip.length-1] = (byte) (startIndex + i);
-                InetAddress subnetAddrInet = InetAddress.getByAddress(subnetAddr);
-                subnets.add(subnetAddrInet.getHostAddress());
-            }
-        } catch (UnknownHostException e) {
-            System.out.println("Invalid IP address or subnet mask.");
-            return ResponseEntity.status(100).body(null);
-        }
-
-         */
-        return ResponseEntity.status(200).body(testSubnets);
+    public ResponseEntity<HashMap<String, String>> calculateSubnetsAndReturnNewGeneratedSubnets(@RequestBody NewCalculateRequest request){
+        String ip = request.ip();
+        int cidr = Integer.parseInt(request.mask());
+        int amount = request.amount();
+        HashMap<String, String> result = calculateSubnets(ip,cidr,amount);
+        return ResponseEntity.status(200).body(result);
     }
 
-    public static List<String> calculateSubnets(String ip, int cidr, int amount){
-        List<String> subnets = new ArrayList<>();
-        System.out.println("CIDR Annotation:" + cidr);
-        System.out.println("Extra reserved bits for hosts:" + amount);
+    public static HashMap<String, String> calculateSubnets(String ip, int cidr, int amount){
+        boolean debug = true;
         String subnetMask = getSubnetByCidr(cidr);
         char[] binaryIp = convertIpStringToCharBinaryArray(ip);
-        System.out.print("IP in Binary: ");
-        for(char x: binaryIp){
-            System.out.print(x);
-        }
-        System.out.println();
         char[] binaryMask = convertIpStringToCharBinaryArray(subnetMask);
-        System.out.print("Mask in Binary: ");
-        for(char y: binaryMask){
-            System.out.print(y);
-        }
-        System.out.println();
+        int newAmount = getAmountOfBitsBorrowed(amount);
+        char[] newBinaryMask = getUpdatedBinaryIpIncludingSubnetAmount(binaryMask, newAmount);
+        int[] x = getStepsToGoForEachSubnet(newBinaryMask);
         char[] wildcard = getWildcard(binaryMask);
         String wildcardString = String.valueOf(wildcard);
-        System.out.print("Wildcard: in binary: ");
         String[] octetsWildcard = new String[4];
         for(int i = 0; i < 4; i++) {
             octetsWildcard[i] = wildcardString.substring(8*i, 8*i + 8);
         }
-        for(char z: wildcard){
-            System.out.print(z);
-        }
-        System.out.println();
         wildcardString = binaryIpToString(octetsWildcard);
-        System.out.println("Wildcard as IP:" + binaryIpToString(octetsWildcard));
-        System.out.println();
         char[] result = getFirstHostAddress(binaryIp,binaryMask);
         String binary = String.valueOf(result);
         String[] octets = new String[4];
         for(int i = 0; i < 4; i++) {
             octets[i] = binary.substring(8*i, 8*i + 8);
         }
-        String answer = binaryIpToString(octets);
-        String broadcastAnswer = getBroadcastIp(answer,wildcardString);
-        subnets.add(answer);
-        subnets.add(broadcastAnswer);
-        //System.out.println("NetzIP: " + answer + "/" + cidr);
-        //System.out.println("BroadcastIP: " + broadcastAnswer);
+
+        if(debug){
+            System.out.println("START SUBNET CALCULATiON: ");
+            System.out.println("CIDR Annotation: " + cidr);
+            System.out.println("Extra reserved bits for hosts: " + amount);
+            System.out.print("IP in Binary: ");
+            for(char z: binaryIp){
+                System.out.print(z);
+            }
+            System.out.println();
+            System.out.print("Mask in Binary: ");
+            for(char z: binaryMask){
+                System.out.print(z);
+            }
+            System.out.println();
+            System.out.print("Updated Mask in Binary: ");
+            for(char z: newBinaryMask){
+                System.out.print(z);
+            }
+            System.out.println();
+            System.out.println("Steps to go: " + x[0]);
+            System.out.println("In Octet: " + x[1]);
+            System.out.println();
+            System.out.println("TEST===============");
+            System.out.print("Wildcard: in binary: ");
+            for(char z: wildcard){
+                System.out.print(z);
+            }
+            System.out.println();
+            System.out.println("Wildcard as IP:" + binaryIpToString(octetsWildcard));
+            System.out.println();
+        }
+
+        String firstNetworkIp = binaryIpToString(octets);
+        String firstBroadcastIp = getBroadcastIp(firstNetworkIp,wildcardString);
+        amount -= 1;
+        System.out.print("Subnet No:1\nNetworkIP: " + firstNetworkIp);
+        System.out.print(", BroadcastIP: " + firstBroadcastIp + "\n");
+        subnets.put(firstNetworkIp, firstBroadcastIp);
+        generateNewSubnets(firstNetworkIp, firstBroadcastIp, amount, x);
         return subnets;
+    }
+
+    public static void generateNewSubnets(String firstNetwork, String firstBroadcast, int amount, int[] x){
+        int index = 2;
+        while(amount > 0) {
+            String newNetworkIp = incrementIp(firstNetwork, x[0], x[1]);
+            String newBroadcastIp = incrementIp(firstBroadcast, x[0],x[1]);
+            System.out.println("Subnet No:" + index);
+            System.out.print("NetworkIP: " + newNetworkIp + ", BroadcastIP: " + newBroadcastIp);
+            System.out.println();
+            firstNetwork = newNetworkIp;
+            firstBroadcast = newBroadcastIp;
+            index++;
+            amount--;
+            subnets.put(firstNetwork, firstBroadcast);
+        }
     }
 
     public static String getBroadcastIp(String ip, String wildcard){
@@ -124,43 +115,123 @@ public class CalculatorController {
             if(i<3) sb.append(".");
         }
 
-        String finalIp = sb.toString();
-        return finalIp;
+        return sb.toString();
     }
 
     public static String binaryIpToString(String[] ip){
-        String res = "";
-        for(int i = 0; i < ip.length; i++) {
+        StringBuilder res = new StringBuilder();
+        for (String s : ip) {
             int result = 0;
-            if (ip[i].charAt(0) == '1') {
+            if (s.charAt(0) == '1') {
                 result += 128;
             }
-            if (ip[i].charAt(1) == '1') {
+            if (s.charAt(1) == '1') {
                 result += 64;
             }
-            if (ip[i].charAt(2) == '1') {
+            if (s.charAt(2) == '1') {
                 result += 32;
             }
-            if (ip[i].charAt(3) == '1') {
+            if (s.charAt(3) == '1') {
                 result += 16;
             }
-            if (ip[i].charAt(4) == '1') {
+            if (s.charAt(4) == '1') {
                 result += 8;
             }
-            if (ip[i].charAt(5) == '1') {
+            if (s.charAt(5) == '1') {
                 result += 4;
             }
-            if (ip[i].charAt(6) == '1') {
+            if (s.charAt(6) == '1') {
                 result += 2;
             }
-            if (ip[i].charAt(7) == '1') {
+            if (s.charAt(7) == '1') {
                 result += 1;
             }
-            res += result + ".";
+            res.append(result).append(".");
         }
-        res = res.substring(0, res.length()-1);
-        return res;
+        res = new StringBuilder(res.substring(0, res.length() - 1));
+        return res.toString();
     }
+
+    public static String incrementIp(String baseIp, int increment, int octet) {
+        String[] octets = baseIp.split("\\.");
+        int currentOctet = Integer.parseInt(octets[octet]);
+        int newOctet = currentOctet + increment;
+        if (newOctet > 255) {
+            System.out.println("Error: Octet value exceeded maximum limit of 255");
+            return baseIp;
+        }
+        octets[octet] = Integer.toString(newOctet);
+        return String.join(".", octets);
+    }
+
+    public static int getAmountOfBitsBorrowed(int amount){
+        while(amount % 2 != 0){
+            amount++;
+        }
+        return switch(amount){
+            case 2 -> 1;
+            case 4 -> 2;
+            case 8 -> 3;
+            case 16 -> 4;
+            case 32 -> 5;
+            case 64 -> 6;
+            case 128 -> 7;
+            case 256 -> 8;
+            default -> 0;
+        };
+    }
+
+    public static char[] getUpdatedBinaryIpIncludingSubnetAmount(char[] binaryMask, int amount){
+        boolean checkForFirstZero = false;
+        int index = 0;
+        while(!checkForFirstZero){
+            if(binaryMask[index] == '0'){
+                checkForFirstZero = true;
+            }
+            index++;
+        }
+        index--;
+        for(int i = 0; i < amount; i++){
+            binaryMask[index+i] = '1';
+        }
+        return binaryMask;
+    }
+
+    public static int[] getStepsToGoForEachSubnet(char[] binaryMask) {
+        int whatOctet = 3;
+        int charAt = 0;
+        for (int i = binaryMask.length - 1; i >= 0; i--) {
+            if (binaryMask[i] == '0') {
+                charAt++;
+                if (charAt >= 8) {
+                    charAt = 0;
+                    whatOctet--;
+                }
+            } else {
+                break;
+            }
+        }
+        int[] x = new int[2];
+        x[0] = getLastBitValue(charAt);
+        x[1] = whatOctet;
+        return x;
+    }
+
+    public static int getLastBitValue(int bitSpot){
+        return switch(bitSpot){
+            case 0 -> 1;
+            case 1 -> 2;
+            case 2 -> 4;
+            case 3 -> 8;
+            case 4 -> 16;
+            case 5 -> 32;
+            case 6 -> 64;
+            case 7 -> 128;
+            case 8 -> 256;
+            default -> 0;
+        };
+    }
+
 
     public static char[] getFirstHostAddress(char[] ip, char[] mask){
         char[] result = new char[ip.length];
@@ -201,9 +272,8 @@ public class CalculatorController {
         }
 
         String joinedString = Arrays.toString(res).replaceAll("[\\[\\], ]", "");
-        char[] charArray = joinedString.toCharArray();
 
-        return charArray;
+        return joinedString.toCharArray();
     }
 
     public static String getSubnetByCidr(int CidrAnnotation){
@@ -219,6 +289,7 @@ public class CalculatorController {
             case 24 -> "255.255.255.0";
             case 23 -> "255.255.254.0";
             case 22 -> "255.255.252.0";
+            case 16 -> "255.255.0.0";
 
             default -> throw new IllegalStateException("Unexpected value: " + CidrAnnotation);
         };
